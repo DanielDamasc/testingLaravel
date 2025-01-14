1013418dc7a58275b8fee2252b70a58b7b88f883
Daniel Damasceno Meira <daniel.meira@campus.unimontes.int>
2025-01-14 18:43:18 -0300

    simple way to transfer components data

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,7 +10,7 @@ class MainController extends Controller
     public function showPage(): View
     {
         $data = [
-            'Dan' => [
+            'Carol' => [
                 'Ingles',
                 'Portugues'
             ],
